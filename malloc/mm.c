7ad676fd9bdbf5e821f38b1b66afe7a8da0b174b
brianclark <2229553982@qq.com>
2025-11-24 22:42:58 +0800

    11.24 mm.c 定义了一组宏来访问和遍历空闲链表

---
+++ malloc/mm.c
@@ -0,0 +1,26 @@
+/* Basic constants and macros */
+#define WSIZE 	   4	  /**/
+#define DSIZE 	   8	  /**/
+#define CHUNKSIZE (1<<12) /**/
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p)      (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p)  (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/* Given block pointer bp, compute address of its header and footer */
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given block pointer bp, compute address of next block and previous block */
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
